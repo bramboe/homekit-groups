6839c9c28f0d910e6d2ae46e8e042aadce96d699
bramboe <37293908+bramboe@users.noreply.github.com>
2026-02-25 22:43:46 +0100

    Update __init__.py

--- homekit_architect/integration/homekit_architect/__init__.py
+++ homekit_architect/integration/homekit_architect/__init__.py
@@ -43,6 +43,7 @@ async def async_setup(hass: HomeAssistant, config: dict[str, Any]) -> bool:
                 "_panel_custom": {
                     "name": "homekit-architect-panel",
                     "js_url": f"/{DOMAIN}_panel/homekit-architect.js",
+                    "embed_iframe": True,
                 },
             },
             require_admin=True,
